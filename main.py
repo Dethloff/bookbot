9e3a4343291840f7da482aea246c8224c0c42409
dethloff <dethloff.cr@gmail.com>
2025-11-13 22:44:17 -0500

    completed the project

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from stats import word_count
+import sys
+from stats import word_count, char_count, sorted_dict
 
 def get_book_text(file_path):
     with open(file_path) as f:
-        content =  f.read()
-    return content
+        return f.read()
 
-def main(get_book_text):
-    book_path = "./books/frankenstein.txt"
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
-    return book_text
+# getting character counts from the book text
+    char_counts = char_count(book_text)
+#sorting the character counts
+    sorted_char_counts = sorted_dict(char_counts)
+#formatting the character counts
+    print("Usage: python3 main.py <path_to_book>")
+    print(f"Analyzing books found at {book_path}...")
+    print(f"Found {word_count(book_text)} total words")
+    print(sorted_char_counts)
 
-word_count(main)
+main()
--- stats.py
+++ stats.py
@@ -1,6 +1,24 @@
-from main import get_book_text
-
-def word_count(main):
-    text = main(get_book_text)
+def word_count(text):
     words = text.split()
-    print(f"Found {len(words)} total words")
+    return len(words)
+
+def char_count(word):
+    words = word.lower()
+    running_count = {}
+    for char in words:
+        if char in running_count:
+            running_count[char] += 1
+        else:
+            running_count[char] = 1
+    return running_count
+
+def sorted_dict(char_count):
+    list_of_dicts = []
+    new_list = []
+    for i in sorted(char_count, key=lambda x: char_count[x], reverse=True):
+        if i.isalpha():
+            list_of_dicts.append({"char": i, "num": char_count[i]})
+            new_list.append(i +": "+ str(char_count[i]))
+    return  new_list
+
+
